c30990a3a35e268bf5745427251cadc91b8cffcd
AdryelleFS <adryellefonsecasousa54@gmail.com>
2026-07-20 14:21:53 -0300

    Add criação de função que cria arquivo
    Adicionado função que cria tanto o arquivo  Relatorio_Processamento.csv quanto o log.txt

---
+++ ProcessarArquivos.ps1
@@ -0,0 +1,102 @@
+# Lista de pastas a serem verificadas
+$pastas = @(
+    "Processados",
+    "Backup",
+    "Logs",
+    "Relatorio"
+)
+
+#Cria as pastas, caso elas ainda não existam 
+
+foreach ($pasta in $pastas) {
+    if (Test-Path $pasta) {
+        Write-Host "A pasta '$pasta' existe."
+    }
+    else {
+        Write-Host "A pasta '$pasta' não existe."
+        foreach ($pasta in $pastas) {
+            New-Item -Path $pasta -ItemType Directory
+            Write-Host "Pasta '$pasta' criada com sucesso."
+        }
+    }
+}
+
+# Move os arquivos .csv para a pasta Processados  
+
+$arquivos = Get-ChildItem -Path "C:\Users\adryelle.sousa\Downloads\*.csv"
+
+foreach ($arquivo in $arquivos) { 
+
+    Move-Item -Path $arquivo.FullName -Destination "C:\Users\adryelle.sousa\Processados"
+    Write-Host "$($arquivo.Name) movido."
+}
+
+# Contabiliza quantos arquivos foram processados, ignorados e o tempo de execução 
+
+$ArquivosProcessados = 0
+$ArquivosIgnorados = 0
+
+$Inicio = Get-Date
+
+foreach ($Arquivo in $Arquivos) {
+
+    if ($Arquivo.Length -gt 0) {
+        $ArquivosProcessados++
+    }
+    else {
+        $ArquivosIgnorados++
+    }
+}
+
+
+# função que cria o arquivo Relatorio_Processamento.cs e o arquivo log.txt
+function CriaArquivo{
+    param (
+        [string]$NomeArquivo,
+        [string]$PastaDestino,   
+        [int]$Tamanho,
+        [string]$Extensao,
+        [int]$QntArquivosProcessados,
+        [int]$QntArquivosIgnorados,
+        [single]$TempoTotalExecucao
+    )
+    Get-Command CriaArquiv**
+
+    $DataProcessamento = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+
+     $Conteudo = @"
+Nome do Arquivo: $NomeArquivo
+Extensão: $Extensao
+Data de Processamento: $DataProcessamento
+Tamanho: $Tamanho
+Pasta de Destino: $PastaDestino
+Arquivos Processados: $ArquivosProcessados
+Arquivos Ignorados: $ArquivosIgnorados
+Tempo de Execução: $TempoExecucao
+"@
+    $ArquivoRelatorio = Join-Path $PastaDestino "$NomeArquivo"
+    Set-Content -Path $ArquivoRelatorio -Value $Conteudo
+    Write-Host "$NomeArquivo criado"
+}
+
+$Fim = Get-Date
+$TempoExecucao = ($Fim - $Inicio).TotalSeconds
+
+# Chamado das funções 
+
+CriaArquivo `
+    -NomeArquivo "Relatorio_Processamento.csv" `
+    -Extensao ".csv" `
+    -PastaDestino "C:\Users\adryelle.sousa\Documents\EstudoGitHub\Relatorio" `
+    -Tamanho "150"
+
+CriaArquivo `
+    -NomeArquivo "log.txt" `
+    -Extensao ".txt" `
+    -PastaDestino "C:\Users\adryelle.sousa\Documents\EstudoGitHub\Logs" `
+    -Tamanho "150" `
+    -QntArquivosProcessados $ArquivosProcessados `
+    -QntArquivosIgnorados $ArquivosIgnorados `
+    -TempoTotalExecucao $TempoExecucao
+
+    
